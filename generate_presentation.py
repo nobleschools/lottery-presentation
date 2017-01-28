27de2a80995d6022b5d8157138db1f7b3cb68a69
Joe Ravesloot <jravesloot@noblenetwork.org>
2017-01-28 14:21:57 -0600

    CS-6 Color body table headers

--- generate_presentation.py
+++ generate_presentation.py
@@ -1,16 +1,16 @@
-#! -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 """
 lottery_presentation/generate_presentation.py
 
 Generates a ppt presentation from csv dump of admission lottery results.
-Takes two positional args:
-*<lottery results csv file>* *<desired output file name>*.
+Call with two positional args; eg.:
+    $ python generate_presentation.py <lottery results csv file> <desired output file name>
 
-Assumes the csv is ordered by status: accepted students first, followed by
-waitlist students.  Column headers expected in the csv:
+Assumes the csv is ordered by status, with accepted students first, followed by
+waitlist students.  Column headers expected in the csv are:
 - id
-- lottery_number
+- lottery_number: will be 'Offered' if student has been accepted, 'WL $xxxx' if student is on wait list
 - first_name
 - last_name
 - Elementary
@@ -22,30 +22,43 @@ import csv
 from queue import Queue
 
 from pptx import Presentation
+from pptx.dml.color import RGBColor
 from pptx.util import Inches
 
 
 class PresentationMaker():
     """
-    :class:`PresentationMaker` is a manager object used to read data from a csv
-    of lottery results and add them to a :class:`pptx.Presentation` object.
+    A a manager object used to read data from a csv
+    of lottery results and add them to a pptx.Presentation object.
 
-    :param infile_name: name of csv lottery results file. Expects the following
-                        column headers:
+    Instantiated with two parameters:
+        infile_name:  name of csv lottery results file. Expects the following
+                      column headers:
                             * id
                             * lottery_number
                             * first_name
                             * last_name
                             * Elementary
 
-    :param outfile_name: filename to save ppt presentation as.
+        outfile_name: filename to save the ppt presentation as.
     """
 
+    ## presentation slide spec
     TEMPLATE_FILENAME = "templates/template.pptx"
     TITLE_SLIDE_LAYOUT_INDEX = 0 # title text in middle of slide
-    BODY_SLIDE_LAYOUT_INDEX = 5 # empty body section
-    MAX_ROWS = 8 # in addition to header row on a body slide
-    NAME_FORMAT = "{last_name}, {first_name}"
+    BODY_SLIDE_LAYOUT_INDEX = 5  # empty body section
+    
+    ## body slide table spec
+    MAX_ROWS     = 8 # in addition to header row on a body slide
+    TOP_MARGIN   = Inches(1.7)
+    LEFT_MARGIN  = Inches(0)
+    TABLE_WIDTH  = Inches(10.0)
+    TABLE_HEIGHT = Inches(0.8) # base height; auto adjusted
+    COL_1_WIDTH  = Inches(3.0)
+    COL_2_WIDTH  = Inches(2.0)
+    COL_3_WIDTH  = Inches(5.0)
+    NAME_FORMAT  = "{last_name}, {first_name}"
+    NOBLE_NAVY   = RGBColor(0, 67, 120)
 
 
     def __init__(self, infile_name, outfile_name):
@@ -62,8 +75,9 @@ class PresentationMaker():
         Read in data from the infile csv, and save a pptx
         under the outfile name.
 
-        Creates two sections (a title and set of following body slides) --
-        one for admitted students, and one for waitlist students.
+        Creates two sections (a "section" being a title slide and set of
+        following body slides) -- one for admitted students,
+        and one for waitlist students.
         """
 
         self._add_title_slide("Admitted Students")
@@ -72,8 +86,8 @@ class PresentationMaker():
 
             reader = csv.DictReader(csvfile)
 
+            # process enrolled slides
             for row in reader:
-                # process enrolled slides
                 if row['lottery_number'] == "Offered":
                     self._add_to_body_queue(row)
                 else:
@@ -93,51 +107,10 @@ class PresentationMaker():
     def _add_title_slide(self, title_string=''):
         """
         Helper function that adds a title slide with title_string text.
-
-        :param title_string: string to use as text on the title slide. Defaults
-                             to an empty string.
         """
 
         slide = self.presentation.slides.add_slide(self.title_layout)
         slide.shapes.title.text = title_string
-
-
-    def _add_body_slide(self):
-        """
-        Helper function that adds a body slide to the presentation, using
-        the rows in the body_queue to fill out the table.
-        """
-
-        body_slide = self.presentation.slides.add_slide(self.body_layout)
-        shapes = body_slide.shapes
-
-        # TODO make consts
-        cols   = 3
-        rows   = self.MAX_ROWS + 1 # allow for header row
-        top    = Inches(1.7)
-        left   = Inches(0)
-        width  = Inches(10.0)
-        height = Inches(0.8)
-
-        table = shapes.add_table(rows, cols, left, top, width, height).table
-
-        table.columns[0].width = Inches(3.0)
-        table.columns[1].width = Inches(2.0)
-        table.columns[2].width = Inches(5.0)
-
-        # fill out header row
-        table.cell(0, 0).text = "Name"
-        table.cell(0, 1).text = "Status"
-        table.cell(0, 2).text = "School"
-
-        row_number = 1 # start after header row
-        while self.body_queue.qsize():
-            row_dict = self.body_queue.get()
-            # table.cell(row,col)
-            table.cell(row_number, 0).text = self.NAME_FORMAT.format(**row_dict)
-            table.cell(row_number, 1).text = "{lottery_number}".format(**row_dict)
-            table.cell(row_number, 2).text = "{Elementary}".format(**row_dict)
-            row_number += 1
 
 
     def _add_to_body_queue(self, row_dict):
@@ -151,6 +124,58 @@ class PresentationMaker():
         self.body_queue.put(row_dict)
         if self.body_queue.qsize() >= self.MAX_ROWS:
             self._add_body_slide()
+
+
+    def _add_body_slide(self):
+        """
+        Helper function that adds a body slide to the presentation, using
+        the rows in the body_queue to fill out the table.
+        """
+
+        body_slide = self.presentation.slides.add_slide(self.body_layout)
+        shapes = body_slide.shapes
+
+        num_cols = 3
+        num_rows = self.MAX_ROWS + 1 # allow for header row
+
+        table = shapes.add_table(
+            num_rows,
+            num_cols,
+            self.LEFT_MARGIN,
+            self.TOP_MARGIN,
+            self.TABLE_WIDTH,
+            self.TABLE_HEIGHT
+        ).table
+
+        table.columns[0].width = self.COL_1_WIDTH
+        table.columns[1].width = self.COL_2_WIDTH
+        table.columns[2].width = self.COL_3_WIDTH
+
+        self._format_table_headers(table)
+
+        row_number = 1 # start after header row
+        while self.body_queue.qsize():
+            row_dict = self.body_queue.get()
+            # table.cell(row,col)
+            table.cell(row_number, 0).text = self.NAME_FORMAT.format(**row_dict)
+            table.cell(row_number, 1).text = "{lottery_number}".format(**row_dict)
+            table.cell(row_number, 2).text = "{Elementary}".format(**row_dict)
+            row_number += 1
+
+
+    def _format_table_headers(self, table):
+        """
+        Helper function to format table headers.
+        """
+
+        for i in range(3):
+            cell = table.cell(0, i)
+            cell.fill.solid()
+            cell.fill.fore_color.rgb = self.NOBLE_NAVY
+
+        table.cell(0, 0).text = "Name"
+        table.cell(0, 1).text = "Status"
+        table.cell(0, 2).text = "School"
 
 
     def _end_body_section(self):
